a8623da4dde1c040f2495a7c6f1b884db40c1254
Marina Migurskaya <marina.migurskaya@gmail.com>
2023-12-11 16:07:14 +0300

    1 задача

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая бесконечно запрашивает целые числа с консоли. Программа завершается
+ при вводе символа ‘q’ или при вводе числа, сумма цифр которого четная. 
+5 12 16 q [STOP] 
+3 45 342 15 [STOP]*/
+
+int NumberSum(int n)
+{
+    int sum=0;
+    while (n>0)
+    {
+        sum=sum + n%10;
+        n=n/10;
+    }
+    return sum;
+}
+void GetNumber()
+{
+int i=0;
+while (i<1)
+{
+int n;
+Console.Write("Введите целое число:");
+string s = Console.ReadLine();
+if (s!="q")
+{
+    n=int.Parse(s);
+    int sum=NumberSum(n);
+    if (sum%2==0)
+   {
+    Console.WriteLine("[STOP]");
+    i++;
+   }
+}
+else
+{
+    Console.WriteLine("[STOP]");
+    i++;
+}
+}
+}
+GetNumber();
